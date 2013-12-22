288ca449bd35272292802c6d6086fb2950d585f9
jablonskim <mjablonski92@gmail.com>
2013-12-22 17:50:49 +0100

    Lista kontrolerow

--- WMS.WebClient/Controllers/HomeController.cs
+++ WMS.WebClient/Controllers/HomeController.cs
@@ -6,8 +6,15 @@ using System.Web.Mvc;
 
 namespace WMS.WebClient.Controllers
 {
+    /// <summary>
+    /// Menu główne aplikacji
+    /// </summary>
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Menu główne i statystyki
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
